d800e7fe072f77b57375ac03387e7ffbe92450b8
Peachie Joyce <peachumsgoblin@pm.me>
2025-12-09 05:22:50 -0800

    Sync

--- perl/backup.pl
+++ perl/backup.pl
@@ -12,11 +12,31 @@ my @repos = (
              '/home/peachie/Shared/',
             );
 
-foreach my $repo (@repos) {
-  say "Changing working directory to $repo";
-  chdir($repo) or die "Cannot change directory: $!\n";
-  say "Pushing latest changes to Github";
-  system("git add .");
-  system("git commit -m 'Sync'");
-  system("git push");
+my $git;
+my $usb;
+my $help;
+
+GetOptions (
+            "git|g" => \$git,
+            "usb|u" => \$usb,
+            "help|h" => \$help,
+            ) or die "Usage: backup [--git|g] [--usb|u] [--help|-h]\n";
+
+if ($help) {
+  say "Usage: backup [--git|g] [--usb|u] [--help|-h]";
+  exit 0;
+} elsif (!defined $git && !defined $usb) {
+  say "Please specify sync location(s)";
+  say "Usage: backup [--git|g] [--usb|u] [--help|-h]";
+};
+
+if ($git) {
+  foreach my $repo (@repos) {
+    say "Changing working directory to $repo";
+    chdir($repo) or die "Cannot change directory: $!\n";
+    say "Pushing latest changes to Github";
+    system("git add .");
+    system("git commit -m 'Sync'");
+    system("git push");
+  }
 }
