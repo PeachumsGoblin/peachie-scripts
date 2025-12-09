b67aeb28183ff328ce4096104e86e70edd591237
Peachie Joyce <peachumsgoblin@pm.me>
2025-12-09 06:23:20 -0800

    Backup from Perl script

--- perl/backup.pl
+++ perl/backup.pl
@@ -6,7 +6,7 @@ use Getopt::Long qw(GetOptions);
 
 my $home = "/home/peachie";
 
-my @repos = (
+my @homerepos = (
              ".dotfiles/",
              "Documents/",
              "Scripts/",
@@ -18,40 +18,65 @@ my @localdirs = (
 
                 );
 
-my @alldirs = (@repos, @localdirs);
-
 my $git;
 my $usb;
 my $help;
 
 GetOptions (
-            "git|g" => \$git,
             "usb|u" => \$usb,
+            "git|g" => \$git,
             "help|h" => \$help,
-            ) or die "Usage: backup [--git|g] [--usb|u] [--help|-h]\n";
+            ) or die "Usage: backup [--usb|u] [--git|g] [--help|-h]\n";
 
 if ($help) {
-  say "Usage: backup [--git|g] [--usb|u] [--help|-h]";
+  say "Usage: backup [--usb|u] [--git|g] [--help|-h]";
   exit 0;
 } elsif (!defined $git && !defined $usb) {
   say "Please specify sync location(s)";
-  say "Usage: backup [--git|g] [--usb|u] [--help|-h]";
+  say "Usage: backup [--usb|u] [--git|g] [--help|-h]";
 };
+
+my $uuid = "UUID=8a135c35-a49c-4c50-9895-6942d1e09753";
+my $mountcheck = "findmnt --source $uuid";
+
+if ($usb) {
+  say "Checking if USB is mounted...";
+  my $mountcheck_output = `$mountcheck`;
+  my $mountcheck_exitcode = $?;
+  if ($mountcheck_exitcode == 0) {
+    print "USB thumbdrive is mounted.\n";
+  } else {
+    print "USB thumbdrive is not mounted.\n";
+    say "Attempting to mount USB thumbdrive...";
+    system("sudo mount $uuid /mnt/thumb");
+    my $mountcheck_output = `$mountcheck`;
+    my $mountcheck_exitcode = $?;
+    if ($mountcheck_exitcode == 0) {
+      print "USB thumbdrive is mounted.\n";
+    } else {
+      print "Mounting USB thumbdrive failed.\n";
+      exit 1;
+    };
+  };
+  say "ヽ(*・ω・)ﾉ Commencing USB backup~!";
+  foreach my $dir (@homerepos) {
+    say "(づ*ᴗ͈ˬᴗ͈)づ*.ﾟ✿ Syncing $dir to USB";
+    system("sudo rsync -avz $home/$dir /mnt/thumb/$dir");
+  };
+  foreach my $dir (@localdirs) {
+    say "(づ*ᴗ͈ˬᴗ͈)づ*.ﾟ✿ Syncing $dir to USB";
+    system("echo sudo rsync -avz $home/$dir /mnt/thumb/$dir");
+  };
+}
 
 if ($git) {
   say "ヽ(*・ω・)ﾉ Commencing Github backup~!";
-  foreach my $repo (@repos) {
-    say "(づ*ᴗ͈ˬᴗ͈)づ*.ﾟ✿ Changing working directory to $repo";
-    chdir("$home/$repo") or die "Cannot change directory: $!\n";
+  foreach my $dir (@homerepos) {
+    say "(づ*ᴗ͈ˬᴗ͈)づ*.ﾟ✿ Changing working directory to $dir";
+    chdir("$home/$dir") or die "Cannot change directory: $!\n";
     say "(づ*ᴗ͈ˬᴗ͈)づ*.ﾟ✿ Pushing latest changes to Github";
     system("git add .");
     system("git commit -m 'Backup from Perl script'");
     system("git push");
-  }
-}
-
-if ($usb) {
-  foreach my $dir (@alldirs) {
-
   }
 }
