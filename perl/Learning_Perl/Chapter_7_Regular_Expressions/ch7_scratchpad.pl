6698feb2416b38808166f6a70a74ca5ea504c402
Peachie Joyce <peachumsgoblin@pm.me>
2025-12-20 23:12:28 -0800

    sync

--- perl/Learning_Perl/Chapter_7_Regular_Expressions/ch7_scratchpad.pl
+++ perl/Learning_Perl/Chapter_7_Regular_Expressions/ch7_scratchpad.pl
@@ -1,3 +1,13 @@
-#! usr/bin/perl
+#! /usr/bin/perl
 use strict;
 use warnings;
+
+while( <STDIN> ) {
+  chomp;
+  if ( /$ARGV[0]/ ) {
+    print "\tMatches\n";
+  }
+  else {
+    print "\tDoesn't match\n";
+  }
+}
